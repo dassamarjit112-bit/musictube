c0c7f0e9648bceb046a81edc5db96b9076c68477
dassamarjit112-bit <dassamarjit112@gmail.com>
2026-03-26 01:37:15 +0530

    66

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MaterialApp(home: YTMApp()));
+}
+
+class YTMApp extends StatefulWidget {
+  const YTMApp({super.key});
+  @override
+  State<YTMApp> createState() => _YTMAppState();
+}
+
+class _YTMAppState extends State<YTMApp> {
+  InAppWebViewController? webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            // Replace with your local IP or hosted URL for testing
+            url: WebUri("http://localhost:5173"), 
+          ),
+          initialSettings: InAppWebViewSettings(
+            mediaPlaybackRequiresUserGesture: false,
+            allowsInlineMediaPlayback: true,
+            iframeAllow: "autoplay",
+            iframeAllowFullscreen: true,
+            useOnLoadResource: true,
+            // Android-specific: Background Playback Support
+            cacheEnabled: true,
+            domStorageEnabled: true,
+          ),
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+          onPermissionRequest: (controller, request) async {
+            return PermissionResponse(
+              resources: request.resources,
+              action: PermissionResponseAction.GRANT,
+            );
+          },
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            var uri = navigationAction.request.url!;
+            if (![ "http", "https", "file", "chrome",
+              "data", "javascript", "about"].contains(uri.scheme)) {
+              if (await canLaunchUrl(uri)) {
+                await launchUrl(uri);
+                return NavigationActionPolicy.CANCEL;
+              }
+            }
+            return NavigationActionPolicy.ALLOW;
+          },
+        ),
+      ),
+    );
+  }
+}
