b87500fa180358a91891291d0aaec5a2890ba4cd
dassamarjit112-bit <dassamarjit112@gmail.com>
2026-03-26 22:42:39 +0530

    fddfb

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,36 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(home: YTMApp()));
 }
 
 class YTMApp extends StatefulWidget {
   const YTMApp({super.key});
-
   @override
   State<YTMApp> createState() => _YTMAppState();
 }
 
 class _YTMAppState extends State<YTMApp> {
-  late final WebViewController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..loadFlutterAsset('assets/www/index.html');
-  }
+  InAppWebViewController? webViewController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: WebViewWidget(controller: controller),
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri("file:///android_asset/flutter_assets/assets/www/index.html"), 
+          ),
+          initialSettings: InAppWebViewSettings(
+            mediaPlaybackRequiresUserGesture: false,
+            allowsInlineMediaPlayback: true,
+            iframeAllow: "autoplay",
+            iframeAllowFullscreen: true,
+            useOnLoadResource: true,
+            cacheEnabled: true,
+            domStorageEnabled: true,
+            transparentBackground: true,
+            // Android-specific settings
+            safeBrowsingEnabled: true,
+            allowFileAccessFromFileURLs: true,
+            allowUniversalAccessFromFileURLs: true,
+            mixedContentMode: MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
+          ),
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+        ),
       ),
     );
   }
